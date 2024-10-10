56caafa7b6f6c1359ddd4a2755b8b05b74b4571b
#Pmc2023ecby47# <msmuhammadsaad78@gmail.com>
2024-10-10 20:40:58 +0500

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>;
+  return <h1 className="text-3xl font-bold min-h-screen text-center mt-20">Welcome to the Home Page</h1>;
 }
